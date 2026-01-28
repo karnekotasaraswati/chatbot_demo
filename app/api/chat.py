fd0003b2e4963437bd45f917ed4175bdc8bd6cdd
unknown <karnekotasaraswati@gmail.com>
2026-01-28 15:33:01 +0530

    faiss disabled

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,69 +1,33 @@
-# from fastapi import APIRouter
-# from pydantic import BaseModel
-# from app.services.llm_service import generate_response
-# # from app.services.retrieval_service import search_context
-
-# router = APIRouter()
-
-# class ChatRequest(BaseModel):
-#     question: str
-
-# @router.post("/chat")
-# async def chat_bot(request: ChatRequest):
-
-
-#     # context = search_context(request.question)
-#     # print("Retrieved Context:", context)
-
-#     prompt = f"""
-# You are StarZopp AI Assistant.
-
-# Answer ONLY using this context.
-# If answer is not found say: I don't know.
-
-# # Context:
-# # {context}
-
-# Question:
-# {request.question}
-# """
-
-#     answer = generate_response(prompt)
-#     print(answer)
-#     return {"answer": answer}
-
 from fastapi import APIRouter
 from pydantic import BaseModel
 from app.services.llm_service import generate_response
+# from app.services.retrieval_service import search_context
 
 router = APIRouter()
 
-
-# Health check endpoint (for Render & monitoring)
-@router.get("/health")
-def health_check():
-    return {"status": "StarZopp API running"}
-
-
-# Request schema
 class ChatRequest(BaseModel):
     question: str
 
-
-# Chat endpoint
 @router.post("/chat")
 async def chat_bot(request: ChatRequest):
 
+
+    # context = search_context(request.question)
+    # print("Retrieved Context:", context)
+
     prompt = f"""
 You are StarZopp AI Assistant.
+
+Answer ONLY using this context.
+If answer is not found say: I don't know.
+
+# Context:
+# {context}
 
 Question:
 {request.question}
 """
 
     answer = generate_response(prompt)
-
-    return {
-        "question": request.question,
-        "answer": answer
-    }
+    print(answer)
+    return {"answer": answer}
