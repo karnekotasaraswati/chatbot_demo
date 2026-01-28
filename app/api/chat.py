c924c6eeba657e7d6de2ce6f3b5159f85ac46fd8
unknown <karnekotasaraswati@gmail.com>
2026-01-28 15:50:05 +0530

    fix context undefined error

--- app/api/chat.py
+++ app/api/chat.py
@@ -8,26 +8,40 @@ router = APIRouter()
 class ChatRequest(BaseModel):
     question: str
 
+# @router.post("/chat")
+# async def chat_bot(request: ChatRequest):
+
+
+#     # context = search_context(request.question)
+#     # print("Retrieved Context:", context)
+
+#     prompt = f"""
+# You are StarZopp AI Assistant.
+
+# Answer ONLY using this context.
+# If answer is not found say: I don't know.
+
+# # Context:
+# # {context}
+
+# Question:
+# {request.question}
+# """
+
+#     answer = generate_response(prompt)
+#     print(answer)
+#     return {"answer": answer}
+
 @router.post("/chat")
 async def chat_bot(request: ChatRequest):
 
-
-    # context = search_context(request.question)
-    # print("Retrieved Context:", context)
-
     prompt = f"""
 You are StarZopp AI Assistant.
-
-Answer ONLY using this context.
-If answer is not found say: I don't know.
-
-# Context:
-# {context}
 
 Question:
 {request.question}
 """
 
     answer = generate_response(prompt)
-    print(answer)
+
     return {"answer": answer}
